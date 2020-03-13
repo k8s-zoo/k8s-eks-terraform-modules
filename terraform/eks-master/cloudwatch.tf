7640e1e5b28e1904c1121e891cbf801a0a3b8f0c
Mishal Shah <mishah@groupon.com>
2020-03-13 21:29:32 +0530

    Adding tags to cw group

--- terraform/eks-master/cloudwatch.tf
+++ terraform/eks-master/cloudwatch.tf
@@ -1,4 +1,11 @@
 resource "aws_cloudwatch_log_group" "master-cluster-cw-loggroup" {
   name              = "/aws/eks/${var.cluster_name}/cluster"
   retention_in_days = var.log_retention_in_days
+
+  tags = {
+    Name  = local.name_prefix
+    owner = var.owner
+    stack = var.stack
+    env   = var.env
+  }
 }
