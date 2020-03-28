3507393480731e121cfa78323cf0f0e49fe9d1b0
Mishal Shah <mishah@groupon.com>
2020-03-28 18:33:58 +0530

    Updating userdata.

--- terraform/eks-ec2-workers/ec2-userdata.sh
+++ terraform/eks-ec2-workers/ec2-userdata.sh
@@ -2,4 +2,12 @@
 
 set -o xtrace
 
+echo "preserve_hostname: true" >> /etc/cloud/cloud.cfg
+hostnamectl set-hostname $(curl -s http://169.254.169.254/latest/meta-data/hostname | cut -d' ' -f2 | cut -d. -f1)
+rm -rf /var/lib/cloud/instances/
+systemctl restart systemd-hostnamed.service
+systemctl restart cloud-init
+
 /etc/eks/bootstrap.sh --apiserver-endpoint '${cluster_endpoint}' --b64-cluster-ca '${cluster_certificate_authority}' '${cluster_name}'
+
+systemctl restart kubelet
