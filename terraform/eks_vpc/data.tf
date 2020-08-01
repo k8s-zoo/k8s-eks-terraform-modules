a5ce1727a9b009581430188619672632767cdea1
mishal <mishalshah92@gmail.com>
2020-08-01 11:55:29 +0530

    Fixing ids.

--- terraform/eks_vpc/data.tf
+++ terraform/eks_vpc/data.tf
@@ -5,9 +5,9 @@ data "aws_region" "current" {}
 data "aws_availability_zones" "current" {}
 
 data "aws_route_table" "eks_public_subnet_rt" {
-  subnet_id = element(aws_subnet.eks_public_subnet, 0)
+  subnet_id = element(aws_subnet.eks_public_subnet.id, 0)
 }
 
-data "aws_route_table" "eks_private_subnet_rt" {
-  subnet_id = element(aws_subnet.eks_private_subnet, 0)
-}
+//data "aws_route_table" "eks_private_subnet_rt" {
+//  subnet_id = element(aws_subnet.eks_private_subnet, 0)
+//}
