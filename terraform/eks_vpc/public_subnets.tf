e4e723a85975b18a96d00f61acda90bed6ca5ee6
mishal <mishalshah92@gmail.com>
2020-08-01 19:09:01 +0530

    Adding more detailed tag

--- terraform/eks_vpc/public_subnets.tf
+++ terraform/eks_vpc/public_subnets.tf
@@ -1,20 +1,21 @@
 ## Public Subnet Setup
 
 resource "aws_subnet" "eks_public_subnet" {
-  count = length(data.aws_availability_zones.current.names)
-  vpc_id = aws_vpc.eks_vpc.id
-  cidr_block = "10.0.${10+count.index}.0/24"
-  availability_zone= element(data.aws_availability_zones.current.names, count.index)
+  count                   = length(data.aws_availability_zones.current.names)
+  vpc_id                  = aws_vpc.eks_vpc.id
+  cidr_block              = "10.0.${10 + count.index}.0/24"
+  availability_zone       = element(data.aws_availability_zones.current.names, count.index)
   map_public_ip_on_launch = true
 
   tags = merge(local.common_tags, {
+    Name = "${local.name_prefix}-public"
     Type = "public",
   })
 }
 
 resource "aws_internet_gateway" "eks_public_ig" {
   vpc_id = aws_vpc.eks_vpc.id
-  tags = local.common_tags
+  tags   = local.common_tags
 }
 
 resource "aws_route_table" "eks_public_subnets_rt" {
@@ -32,7 +33,7 @@ resource "aws_route_table" "eks_public_subnets_rt" {
 }
 
 resource "aws_route_table_association" "eks_public_rt_association" {
-  count = length(aws_subnet.eks_public_subnet.*.id)
+  count          = length(aws_subnet.eks_public_subnet.*.id)
   subnet_id      = element(aws_subnet.eks_public_subnet.*.id, count.index)
   route_table_id = aws_route_table.eks_public_subnets_rt.id
 }
