7a4e9c5e664a29592c695135fe8c718b0e3e6dc8
mishal <mishalshah92@gmail.com>
2020-08-09 00:32:27 +0530

    Adding tags

--- terraform/eks_vpc/public_subnets.tf
+++ terraform/eks_vpc/public_subnets.tf
@@ -8,9 +8,9 @@ resource "aws_subnet" "eks_public_subnet" {
   map_public_ip_on_launch = true
 
   tags = merge(local.common_tags, {
-    Name                       = "${local.name_prefix}-public"
-    Type                       = "public",
-    kubernetes.io / role / elb = 1
+    Name                     = "${local.name_prefix}-public"
+    Type                     = "public",
+    "kubernetes.io/role/elb" = 1
   })
 }
 
